d6badfb00644bddc1ce81839e8fc69ba73c09c29
Ferdi <dervieuxferdinand@gmail.com>
2020-05-15 11:53:56 +0200

    Basic AR Scene
    URP setup
    AR Foundation
    AR Core
    Plane detection

---
+++ CDE/Assets/Editor/HCM/HCM_Data.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class HCM_Data : ScriptableObject {
+	[System.Serializable]
+	public class TagColor {
+		public string Tag = "Untagged";
+		public Color Color = Color.white;
+	}
+	public List<TagColor> TagColors;
+}
